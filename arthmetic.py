a4b69c05a3ae6b9f8986aebee408240627142ae4
AirSupan <airsupansa23@gmail.com>
2022-11-04 13:01:15 +0700

    updata

---
+++ arthmetic.py
@@ -0,0 +1,16 @@
+#python Operator
+x=15
+y=4
+
+#1.Addition
+print('x + y = ', x + y)
+#2.
+print('x - y =', x - y)
+#
+print('x * y=', x * y)
+#
+print('x / y =',x / y)
+#
+print('x % y=', x % y)
+#
+print('x ** y', x ** y)
